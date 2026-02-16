ee14c4da7bb7c99de82bf17e33c742478defd276
Abdullah Ibne Atiq <hello@abdu.io>
2026-02-16 23:53:15 +0000

    Configure url prefix

--- tarawih/tarawih/urls.py
+++ tarawih/tarawih/urls.py
@@ -14,13 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
+
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path,include
 
+URL_PREFIX = os.environ.get("URL_PREFIX", "")
+
 urlpatterns = [
     path("admin/", admin.site.urls),#=-- admin page
-    path("",include('tarawihapp.urls')), #-- include the urls of the app
+    path(URL_PREFIX,include('tarawihapp.urls')), #-- include the urls of the app
 
 ]
 
